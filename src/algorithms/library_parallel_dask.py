7fe66b13338f2d4d74b4518cc3a7db5a5c959016
Hysa Mello <mellohysa7@gmail.com>
2021-12-09 22:41:07 +0100

    delayed working

--- src/algorithms/library_parallel_dask.py
+++ src/algorithms/library_parallel_dask.py
@@ -1,13 +1,26 @@
 import numpy as np
 import time
+<<<<<<< Updated upstream
 from polyleven import levenshtein
+=======
+from Levenshtein import distance
+from multiprocessing import cpu_count
+>>>>>>> Stashed changes
 
 from utils import log_alg_start, log_alg_time
 
 import dask.array as da
+import dask
+import dask.bag as db
+
+# @dask.delayed
+def fuc(a,b):
+    c = da.frompyfunc(distance, 2, 1).outer(a, b)
+    return c
 
 
 def use_levenshtein_library_parallel_dask(input_array) -> np.array:
+<<<<<<< Updated upstream
     log_alg_start('parallel implementation with dask')
 
     start = time.perf_counter()
@@ -17,3 +30,32 @@ def use_levenshtein_library_parallel_dask(input_array) -> np.array:
     log_alg_time(time.perf_counter() - start)
 
     return matrix
+=======
+
+    log_alg_start('python-levenshtein with dask library methods')
+
+    start = time.perf_counter()
+    
+    array_size = len(input_array)
+    ncpus = cpu_count()
+    # equal_pieces = np.array_split(input_array, ncpus)
+    equal_pieces = db.from_sequence(input_array, npartitions=ncpus)
+    
+    pieces_matrix = []
+    for piece in equal_pieces:
+        result = dask.delayed(fuc)(input_array, piece)
+        pieces_matrix.append(result)    
+       
+    final = np.empty([0,array_size], int)
+    
+    total = dask.delayed(np.append)(final, pieces_matrix, axis=0)
+    # print(total.compute())
+    
+    # matrix = da.frompyfunc(distance, 2, 1).outer(input_array, input_array)
+    
+    
+    log_alg_time(time.perf_counter() - start)
+    
+    return total.compute()
+    # return matrix
+>>>>>>> Stashed changes
