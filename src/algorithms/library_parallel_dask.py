3ac5510ff033c7093a7a285f9210ccddc216e6ba
Pedro Rosa <rosapedro10@gmail.com>
2021-12-10 13:53:52 +0100

    fixes

--- src/algorithms/library_parallel_dask.py
+++ src/algorithms/library_parallel_dask.py
@@ -12,19 +12,25 @@ def use_levenshtein_library_parallel_dask(input_array) -> np.array:
 
     start = time.perf_counter()
     
-    matrix = da.frompyfunc(levenshtein, 2, 1).outer(input_array, input_array) 
+    ## First Method
+    # matrix = da.frompyfunc(levenshtein, 2, 1).outer(input_array, input_array) 
     
     
-    # distance = da.frompyfunc(levenshtein, 2, 1)
+    distance = da.frompyfunc(levenshtein, 2, 1)
     
+    
+    # Second Method
     # size = input_array.size
     # a = da.from_array(input_array, chunks=(size))
-    # b = a.map_blocks(lambda x: distance(x[:, None], x), dtype=str, chunks=(size,size))
+    # b = a.map_blocks(lambda x: distance(x[:, None], x), dtype=str, chunks=((size,size),))
     # matrix = b.compute()
     
-    # w = da.from_array(input_array, chunks=(2000))
-    # z = da.blockwise(distance.outer, 'ij', w, 'i', w, 'j', dtype='f8')
-    # matrix = z.compute()
+    
+    ## Third Method
+    w = da.from_array(input_array, chunks=(2500))
+    z = da.blockwise(distance.outer, 'ij', w, 'i', w, 'j', dtype='f8')
+    # matrix = z.compute(scheduler='processes')
+    matrix = z.compute()
     
     
     log_alg_time(time.perf_counter() - start)
