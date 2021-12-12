69ff72cd84aaeba5acfe24b6ca8495af557dc198
Pedro Rosa <rosapedro10@gmail.com>
2021-12-12 22:37:55 +0100

    create and change name of dask scripts

---
+++ src/algorithms/library_parallel_dask_method_1.py
@@ -0,0 +1,38 @@
+import numpy as np
+import time
+from polyleven import levenshtein
+
+from utils import log_alg_start, log_alg_time
+
+import dask.array as da
+
+
+def use_levenshtein_library_parallel_dask(input_array) -> np.array:
+    log_alg_start('parallel implementation with dask')
+
+    start = time.perf_counter()
+    
+    ## First Method
+    # matrix = da.frompyfunc(levenshtein, 2, 1).outer(input_array, input_array) 
+    
+    
+    distance = da.frompyfunc(levenshtein, 2, 1)
+    
+    
+    # Second Method
+    # size = input_array.size
+    # a = da.from_array(input_array, chunks=(size))
+    # b = a.map_blocks(lambda x: distance(x[:, None], x), dtype=str, chunks=((size,size),))
+    # matrix = b.compute()
+    
+    
+    ## Third Method
+    w = da.from_array(input_array, chunks=(2000))
+    z = da.blockwise(distance.outer, 'ij', w, 'i', w, 'j', dtype='f8')
+    matrix = z.compute(scheduler='processes')
+    # matrix = z.compute()
+    
+    
+    log_alg_time(time.perf_counter() - start)
+
+    return matrix
